d2a466c3029ae7bd09c5520906c05c8003797483
Michael Coons <michael@Michaels-MacBook-Pro-3.local>
2018-07-31 19:57:56 -0600

    upated readme for turn in

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 
 module.exports = {
 
@@ -11,50 +9,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
-
   }
 
 };
-
-// module.exports = {
-
-//   development: {
-//     client: 'sqlite3',
-//     connection: {
-//       filename: './dev.sqlite3'
-//     }
-//   },
-
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'resolution',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-
-//   production: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'resolution',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-
-// };
--- queries.js
+++ queries.js
@@ -5,15 +5,10 @@ module.exports = {
         return database('resolution').select();
     },
     read(id){
-        // return database('resolution').where('id','=',id).select().then(response => response[0]);
         return database('resolution').where('id','=',id).first();
     },
     create(resolution){
         return  database('resolution').insert(resolution).returning('*').then( record => record[0]);
-        // console.log("create res: ",res);
-        // let id = database('resolution').returning('id').insert(resolution)
-
-        // return database('resolution').where('id','=',id[0]).select().then(response => response[0]);
     },
     update(id, resolution){
         return database('resolution').where('id','=',id).returning('id').update(resolution);
